18f48af7752feee754f608494f5a023d13684636
Manisha Yewle <manishayewle87-hue@github.com>
2026-08-20 00:44:53 +0530

    chore: cleanup test scripts

--- scripts/test-pseo.mjs
+++
@@ -1,8 +0,0 @@
-import { getPSEOPageData } from '../src/lib/pseo-data.ts';
-import { getDomainConfig } from '../src/lib/domain-config.ts';
-
-const data = getPSEOPageData('mahalaxmi-kohinoor-pimpri');
-console.log('Data:', data);
-
-const cfg = getDomainConfig('kohinoorthearena.in');
-console.log('Cfg:', cfg.arenaName);
